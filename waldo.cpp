5e585beedc00460193289d117985a5247ba52cf0
StamatisKyp <kypstamm@gmail.com>
2025-05-27 11:00:59 -0400

    waldo.cpp

--- waldo.cpp
+++ waldo.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 using namespace std;
 
-void print(vector<vector<int>> b) {
+// Algorithm that goes through an n*n board, and finds the biggest number in the column and the smallest in the row.
+
+void print(vector<vector<int>> b) { // print the board
     for (int i = 0; i < b.size(); i++) {
         for (int j = 0; j < b[i].size(); j++) {
             cout << b[i][j] << " ";
@@ -12,7 +14,7 @@ void print(vector<vector<int>> b) {
 }
 
 
-bool isBiggerAndSmaller(vector<vector<int>> &b, int row, int col) {
+bool isBiggerAndSmaller(vector<vector<int>> &b, int row, int col) { // boolean function that is trying find the biggest in column and smallest in row
     int num = b[row][col];
     int size = b.size();
     for (int i = 0; i < size; i++) {
@@ -28,7 +30,7 @@ bool isBiggerAndSmaller(vector<vector<int>> &b, int row, int col) {
     return true;
 }
 
-bool goThroughTheBoard(vector<vector<int>> &b) {
+bool goThroughTheBoard(vector<vector<int>> &b) { // this function goes through the board, implements the above function, and if found, returns the result
     bool found = false;
     int size = b.size();
     for (int i = 0; i < size; i++) {
@@ -43,37 +45,39 @@ bool goThroughTheBoard(vector<vector<int>> &b) {
 }
 
 int main() {
-    int size;
-    string input;
-    vector <vector <int>> b;
-    start: cout << "Tell me the size of the board: ";
-    cin >> size;
-    while (size >= 100 || size <= 0) {
-        cout << "Error! Please enter a valid board range: ";
+    int size; // size of board
+    string input; // Y/N input for later
+    do {
+        vector<vector<int>> b; // board
+
+        cout << "Tell me the size of the board: ";
         cin >> size;
-    }
-    int arr[size][size];
-    cout << "Give me the numbers: ";
-    for (int i = 0; i < size; i++) {
-        vector <int> row1;
-        for (int j = 0; j < size; j++) {
-            cin >> arr[i][j];
-            row1.push_back(arr[i][j]);
+        while (size >= 100 || size <= 0) { // checks for valid range
+            cout << "Error! Please enter a valid board range: ";
+            cin >> size;
         }
-        b.push_back(row1);
-    }
-    print(b);
-    if (!goThroughTheBoard(b)) {
-        cout << "No solutions!" << endl;
-    }
-    cout << "Do you wish to try again? (y/n): ";
-    cin >> input;
-    if (input == "y" || input == "Y") {
-        goto start;
-    } else {
-        cout << "Terminating...";
-        return 0;
-    }
+
+        cout << "Give me the numbers: "; 
+        for (int i = 0; i < size; i++) { // initializes the matrix
+            vector<int> row1;
+            for (int j = 0; j < size; j++) {
+                int num;
+                cin >> num;
+                row1.push_back(num);
+            }
+            b.push_back(row1);
+        }
+
+        print(b); // prints the board
+        if (!goThroughTheBoard(b)) { // if it doesn't find any number that the biggest in C and smalliest in R
+            cout << "No solutions!" << endl;
+        }
+
+        cout << "Do you wish to try again? (y/n): ";
+        cin >> input;
+    } while (input == "y" || input == "Y");
+
+    cout << "Terminating..." << endl;
     return 0;
 }
 
